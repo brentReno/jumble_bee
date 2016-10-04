a67ad7406ebc44daec57f4c33e86eed2b2e8208d
Brent Reno <brent.reno@gmail.com>
2016-10-04 15:52:55 -0500

    Number of words and Name are collected on click

--- server/public/scripts/controllers/createTestCtrl.js
+++ server/public/scripts/controllers/createTestCtrl.js
@@ -1,3 +1,13 @@
+var numWords;
+var quizName;
 myApp.controller("createTestController", ["$scope", function($scope){
   console.log("This is Create");
+  $scope.showInputs = function(){
+    console.log("in showInputs");
+    //collect the number of words
+    numWords = $scope.numWordsIn;
+    quizName = $scope.nameIn;
+    console.log(numWords,quizName);
+    //collect the name of the quiz
+  };
 }]);
