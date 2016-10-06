3596b29048478f34607b7ffc1f1aa2cc712ed458
Brent Reno <brent.reno@gmail.com>
2016-10-06 08:58:32 -0500

    refactored jumble and shuffle functions into service

--- server/public/scripts/controllers/playGameCtrl.js
+++ server/public/scripts/controllers/playGameCtrl.js
@@ -3,32 +3,6 @@ myApp.controller("playGameController", ["$scope", "quizService", function($scope
   console.log("This is Play ");
   $scope.quizWords = quizService.getQuiz();
   console.log($scope.quizWords);
-  jumble($scope.quizWords);
+  quizService.jumble($scope.quizWords);
   $scope.jumbledWords=jumbledWords;
 }]);
-  //jumble the quiz words
-  var jumble = function(quiz){
-    //loop through quiz data
-    for (var i = 0; i < quiz.length; i++) {
-      // get single word returned in an array
-      var wordToJumble= quiz.slice(i,i+1);
-      var arrayOfLetters= wordToJumble[0].split("");
-      //shuffle the letters
-      shuffle(arrayOfLetters);
-      //join letters in a word
-      var jumbledWord = arrayOfLetters.join("");
-      //push words to an array
-      jumbledWords.push(jumbledWord);
-    }
-  };//end jumble
-
-  //shuffle code from here http://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array-in-javascript
-  var shuffle = function(array){
-    var j,x,i;
-    for (i = array.length; i; i--) {
-      j= Math.floor(Math.random()*i);
-      x = array[i-1];
-      array[i-1] = array[j];
-      array[j]=x;
-    }
-  };//end shuffle
--- server/public/scripts/services/quizService.js
+++ server/public/scripts/services/quizService.js
@@ -7,13 +7,43 @@ myApp.service('quizService',function(){
      words = quiz;
      return words;
   };
-//get the words for manipulation
-var getQuiz = function(){
+  //get the words for manipulation
+  var getQuiz = function(){
   return words;
 };
+
+  //jumble the quiz words
+  var jumble = function(quiz){
+    //loop through quiz data
+    for (var i = 0; i < quiz.length; i++) {
+      // get single word returned in an array
+      var wordToJumble= quiz.slice(i,i+1);
+      var arrayOfLetters= wordToJumble[0].split("");
+      //shuffle the letters
+      shuffle(arrayOfLetters);
+      //join letters in a word
+      var jumbledWord = arrayOfLetters.join("");
+      //push words to an array
+      jumbledWords.push(jumbledWord);
+    }
+    return jumbledWords;
+  };//end jumble
+
+  //shuffle code from here http://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array-in-javascript
+  var shuffle = function(array){
+    var j,x,i;
+    for (i = array.length; i; i--) {
+      j= Math.floor(Math.random()*i);
+      x = array[i-1];
+      array[i-1] = array[j];
+      array[j]=x;
+    }
+    return array;
+  };//end shuffle
   return{
     addWords: addWords,
-    getQuiz: getQuiz
+    getQuiz: getQuiz,
+    jumble: jumble,
+    shuffle: shuffle
   };
-
 });
