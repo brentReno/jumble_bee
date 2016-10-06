612feb11e1d4af57a2a3b97832217091ddb782e8
Brent Reno <brent.reno@gmail.com>
2016-10-06 15:00:18 -0500

    datamuse searches based on nouns and adjectives

--- server/public/scripts/controllers/randomTestCtrl.js
+++ server/public/scripts/controllers/randomTestCtrl.js
@@ -1,13 +1,15 @@
-
+var dataMuseReq;
 myApp.controller("randomTestController", ["$scope","$http", function($scope,$http){
   console.log("This is Random");
+  //populate selects
 
   //random quiz click
   $scope.randomQuiz = function(){
     //collect info
-    var subject = $scope.subject;
+    var subject = $scope.testSubject;
     console.log(subject);
-    var dataMuseReq = 'words?rel_jjb='+subject;
+    if($scope.limiter == "Noun"){
+     dataMuseReq = 'words?rel_jjb='+subject;
     $http({
       method:"GET",
       url:"https://api.datamuse.com/" + dataMuseReq
@@ -15,7 +17,17 @@ myApp.controller("randomTestController", ["$scope","$http", function($scope,$htt
     }).then(function(data){
       console.log(data);
     });
+}
+else if($scope.limiter =="Adjective"){
+     dataMuseReq = 'words?rel_jja='+subject;
+    $http({
+      method:"GET",
+      url:"https://api.datamuse.com/" + dataMuseReq
 
+    }).then(function(data){
+      console.log(data);
+    });
+}
 
   };//end random quiz click
 
