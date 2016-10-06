a7a9db47306fd81926d508a8607684d3d6f639fe
Brent Reno <brent.reno@gmail.com>
2016-10-06 09:39:31 -0500

    on click is pulling value from input field

--- server/public/scripts/controllers/playGameCtrl.js
+++ server/public/scripts/controllers/playGameCtrl.js
@@ -1,8 +1,23 @@
 var jumbledWords=[];
 myApp.controller("playGameController", ["$scope", "quizService", function($scope, quizService){
   console.log("This is Play ");
+  //get quiz words to jumble
   $scope.quizWords = quizService.getQuiz();
+  //get quiz words, as answer key
+  $scope.correctAnswers = quizService.getQuiz();
   console.log($scope.quizWords);
+
+  //jumble the quiz words for display on the Dom
   quizService.jumble($scope.quizWords);
   $scope.jumbledWords=jumbledWords;
+
+  //check answers function
+  $scope.checkAnswer = function(index){
+    var id = "question-"+index;
+    console.log(id);
+    var answer =document.getElementById(id).value;
+    console.log(answer);
+
+  };
+
 }]);
