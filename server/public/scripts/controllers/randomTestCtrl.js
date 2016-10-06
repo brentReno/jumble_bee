2629864bfb22cb3212200df1caa347019ab9d755
Brent Reno <brent.reno@gmail.com>
2016-10-06 17:20:48 -0500

    adjective route is also saving quiz to db

--- server/public/scripts/controllers/randomTestCtrl.js
+++ server/public/scripts/controllers/randomTestCtrl.js
@@ -2,8 +2,7 @@ var dataMuseReq;
 var randomTest=[];
 myApp.controller("randomTestController", ["$scope","$http", "$location", "quizService", function($scope,$http, $location, quizService){
   console.log("This is Random");
-  //populate selects
-
+  $scope.user = JSON.parse( localStorage.getItem( 'userProfile' ));
   //random quiz click
   $scope.randomQuiz = function(){
     //collect info
@@ -12,29 +11,11 @@ myApp.controller("randomTestController", ["$scope","$http", "$location", "quizSe
     console.log(subject);
     if($scope.limiter == "Noun"){
      dataMuseReq = 'words?rel_jjb='+subject+"&max="+ maxNum;
-    $http({
-      method:"GET",
-      url:"https://api.datamuse.com/" + dataMuseReq
+      $http({
+        method:"GET",
+        url:"https://api.datamuse.com/" + dataMuseReq
 
-    }).then(function(data){
-      console.log(data);
-      for (var i = 0; i < data.data.length; i++) {
-        var word = data.data[ i ].word;
-        console.log(word);
-        randomTest.push(word);
-      }
-      randomTest = quizService.addWords(randomTest);
-      console.log("these are going to the service:", randomTest);
-      $location.url('/playGame');
-    });
-}
-else if($scope.limiter =="Adjective"){
-     dataMuseReq = 'words?rel_jja='+subject +"&max="+maxNum;
-    $http({
-      method:"GET",
-      url:"https://api.datamuse.com/" + dataMuseReq
-
-    }).then(function(data){
+      }).then(function(data){
         console.log(data);
         for (var i = 0; i < data.data.length; i++) {
           var word = data.data[ i ].word;
@@ -44,9 +25,37 @@ else if($scope.limiter =="Adjective"){
         randomTest = quizService.addWords(randomTest);
         console.log("these are going to the service:", randomTest);
         $location.url('/playGame');
+      });
+    }
+    else if($scope.limiter =="Adjective"){
+       dataMuseReq = 'words?rel_jja='+subject +"&max="+maxNum;
+      $http({
+        method:"GET",
+        url:"https://api.datamuse.com/" + dataMuseReq
 
-    });
-}
+      }).then(function(data){
+          console.log(data);
+          for (var i = 0; i < data.data.length; i++) {
+            var word = data.data[ i ].word;
+            console.log(word);
+            randomTest.push(word);
+          }
+          var quizName= "Random " + subject + " quiz";
+          var objectToSend ={ quiz_name:quizName, quiz:randomTest, username: $scope.user.username};
+          console.log(objectToSend);
+          $http({
+            method:"POST",
+            url:"/create",
+            data: objectToSend
+          }).then(function(data){
+                console.log("back from the server", data);
+                var quizWords = data.data.words;
+                console.log("quizWords:", quizWords);
+                addingWords = quizService.addWords(quizWords);
+                console.log("these are going to the service:", addingWords);
+                $location.url('/playGame');
+        });
+      });
+    }
   };//end random quiz click
-
 }]);
