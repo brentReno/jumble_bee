c85bfbd78fe702fb320aed153d6ad4e12735d8e9
Brent Reno <brent.reno@gmail.com>
2016-10-06 14:34:57 -0500

    requests to data muse are working

--- server/public/scripts/controllers/randomTestCtrl.js
+++ server/public/scripts/controllers/randomTestCtrl.js
@@ -1,3 +1,22 @@
-myApp.controller("randomTestController", ["$scope", function($scope){
+
+myApp.controller("randomTestController", ["$scope","$http", function($scope,$http){
   console.log("This is Random");
+
+  //random quiz click
+  $scope.randomQuiz = function(){
+    //collect info
+    var subject = $scope.subject;
+    console.log(subject);
+    var dataMuseReq = 'words?rel_jjb='+subject;
+    $http({
+      method:"GET",
+      url:"https://api.datamuse.com/" + dataMuseReq
+
+    }).then(function(data){
+      console.log(data);
+    });
+
+
+  };//end random quiz click
+
 }]);
